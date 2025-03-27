22b7b17c1f8edd0f71193bc282d75d763ec0d0ca
Eric Rowles <ericjacobr1999@gmail.com>
2025-03-26 22:38:48 -0600

    Update some modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@primevue/nuxt-module'
+  ],
   eslint: {
     config: {
       stylistic: true
+    }
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
     }
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description
+})
+</script>
+
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
-  </div>
+  <Button label="Verify" />
+  <ContentRenderer v-if="home" :value="home" />
+  <div v-else>Home not found</div>
 </template>
-  
