d6db52a49b32ccfbbdd93ad385305ffd758b6692
Eric Rowles <ericjacobr1999@gmail.com>
2025-03-26 21:33:23 -0600

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import pluginVue from 'eslint-plugin-vue';
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+)
